477ec24e6a5c7a86d3dea53c98b1be78e52c6c10
Milton Orellana <go0211032020@unab.edu.sv>
2020-12-11 11:39:03 -0600

    Creando los atributos de la clase Ventas

--- ProyectoFinal/src/main/java/com/Lopez/ProyectoFinal/Entidades/Venta.java
+++ ProyectoFinal/src/main/java/com/Lopez/ProyectoFinal/Entidades/Venta.java
@@ -1,5 +1,33 @@
 package com.Lopez.ProyectoFinal.Entidades;
 
-public class Venta {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+
+	
+	@Entity
+	@Data
+	@Table (name="ventas")
+	
+	public class Venta implements Serializable {
+		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		 
+		@Id
+		@GeneratedValue (strategy = GenerationType.IDENTITY)
+		private long id_venta;
+		private String producto;
+		private String precio;
+		private String cantidad;
 
 }
--- ProyectoFinal/src/main/java/com/Lopez/ProyectoFinal/Web/ControladorInico.java
+++ ProyectoFinal/src/main/java/com/Lopez/ProyectoFinal/Web/ControladorInico.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.Lopez.ProyectoFinal.DAO.VentaDAO;
 
-@Controller // Controla los Recursos de Laweb
+@Controller // Controla los Recursos de La web
 public class ControladorInico {
 	@Autowired // trae todas las clases de SQL
 	private VentaDAO VenDAO;
@@ -15,7 +15,7 @@ public class ControladorInico {
 	@GetMapping ("/") //Mapea el sitio web
 	public String Inicio(Model m){
 		
-		var MostrarVentas = VenDAO.findAll();
+		var MostrarVentas = VenDAO.findAll(); //aplicamos el findAll
 		
 		m.addAttribute("Venta", MostrarVentas);
 		
